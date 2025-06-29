cefb147927f14e101621cef2e15183cd564ab3a7
Armando Rodriguez <armandospxp@gmail.com>
2025-06-29 16:29:10 -0300

    Iniciacion del proyecto

---
+++ proyecto1.py
@@ -0,0 +1,20 @@
+##########################################
+
+PROYECTO 1  Aplicaciones de IA
+
+Aprendizaje Supervisado
+
+
+Nombre: Armando Rodriguez
+
+
+email: armandospxp@gmail.com
+
+
+########################################
+
+
+###Importacion de librerias
+
+import numpy as np
+
